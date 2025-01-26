aba85ca892cd8626ec45e716400c9af0504aeac2
kasztelmacko <m.kasztelani@student.uw.edu.pl>
2025-01-26 11:23:03 +0100

    theme changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#4F46E5', // A vibrant purple for primary actions
-        secondary: '#10B981', // A green for secondary actions
-        accent: '#F59E0B', // An orange for accents
-        background: '#F9FAFB', // A light gray for backgrounds
-        text: '#1F2937', // A dark gray for text
+        primary: '#22333b',
+        secondary: '#10B981',
+        accent: '#F59E0B',
+        background: '#FFFFF',
+        text: '#0a0908',
       },
       fontFamily: {
         sans: ['Inter', 'sans-serif'], // Use Inter as the default font
