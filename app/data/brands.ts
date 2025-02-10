3c7078d872d2461f54a7f28037988eee82afd1f4
kasztelmacko <m.kasztelani@student.uw.edu.pl>
2025-02-10 19:12:15 +0100

    improved question id: 9

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -4,33 +4,41 @@ export const brands = {
     mcdonalds: {
       main_color: '#6B7566',
       brand_logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/1200px-McDonald%27s_Golden_Arches.svg.png',
+      brand_pic: 'https://kkpcuktyelbwgigadulx.supabase.co/storage/v1/object/public/item_photos//mcdonalds_pic.png'
     },
     burger_king: {
       main_color: '#512314',
       brand_logo: 'https://logos-world.net/wp-content/uploads/2020/05/Burger-King-Logo.png',
+      brand_pic: 'https://kkpcuktyelbwgigadulx.supabase.co/storage/v1/object/public/item_photos//burger_king_pic.png'
     },
     kfc: {
       main_color: '#ED1C24',
-      brand_logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/bf/KFC_logo.svg/1200px-KFC_logo.svg.png',
+      brand_logo: 'https://logos-world.net/wp-content/uploads/2020/04/KFC-Logo.png',
+      brand_pic: 'https://kkpcuktyelbwgigadulx.supabase.co/storage/v1/object/public/item_photos//kfc_pic.png'
     },
     subway: {
       main_color: '#009933',
-      brand_logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Subway_Logo.svg/1200px-Subway_Logo.svg.png',
+      brand_logo: 'https://logos-world.net/wp-content/uploads/2023/01/Subway-Logo.png',
+      brand_pic: 'https://kkpcuktyelbwgigadulx.supabase.co/storage/v1/object/public/item_photos//subway_pic.png'
     },
     max_burger: {
       main_color: '#ee141f',
-      brand_logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Max_%28Restaurant%29_logo.svg/150px-Max_%28Restaurant%29_logo.svg.png'
+      brand_logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Max_%28Restaurant%29_logo.svg/150px-Max_%28Restaurant%29_logo.svg.png',
+      brand_pic: 'https://kkpcuktyelbwgigadulx.supabase.co/storage/v1/object/public/item_photos//max_burger_pic.png'
     },
     wendys: {
       main_color: "#de0c35",
-      brand_logo: "https://logos-world.net/wp-content/uploads/2021/11/Wendys-Logo-700x394.png"
+      brand_logo: "https://logos-world.net/wp-content/uploads/2021/11/Wendys-Logo-700x394.png",
+      brand_pic: 'https://kkpcuktyelbwgigadulx.supabase.co/storage/v1/object/public/item_photos//wendys_pic.png'
     },
     popeyes: {
       main_color: "##ff7d00",
-      brand_logo: "https://logos-world.net/wp-content/uploads/2021/10/Popeyes-Logo-700x394.png"
+      brand_logo: "https://logos-world.net/wp-content/uploads/2021/10/Popeyes-Logo-700x394.png",
+      brand_pic: 'https://kkpcuktyelbwgigadulx.supabase.co/storage/v1/object/public/item_photos//popeyes_pic.png'
     },
     taco_bell: {
       main_color: "#5b2994",
-      brand_logo: "https://logos-world.net/wp-content/uploads/2021/10/Taco-Bell-Logo-700x394.png"
+      brand_logo: "https://logos-world.net/wp-content/uploads/2021/10/Taco-Bell-Logo-700x394.png",
+      brand_pic: 'https://kkpcuktyelbwgigadulx.supabase.co/storage/v1/object/public/item_photos//taco_bell_pic.png'
     }
   };
